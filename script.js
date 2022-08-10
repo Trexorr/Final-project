713672799866e426ca96a5d51832a0f3cb8580f3
Tristen Teixeira <trexorr@me.com>
2022-08-10 22:04:44 +1000

    Final Draft

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+                            // Declarations
+
+                            
+// Class selector
+
+// let divs = ["traditional", "blacknGold", "greennGold", "dustyRose"];
+// let visibleId = null;
+
+const cardinv = document.querySelector('#invCard')
+const trad = document.querySelector('.style1')
+const black = document.querySelector('.style2')
+const green = document.querySelector('.style3')
+const rose = document.querySelector('.style4')
+const one = document.querySelector('#out1')
+
+
+
+
+// Input
+const text1 = document.getElementById('in1');
+const text2 = document.getElementById('in2');
+const text3 = document.getElementById('in3');
+const text4 = document.getElementById('in4');
+const text5 = document.getElementById('in5');
+const output1 = document.querySelector('#out1')
+const output2 = document.querySelector('#out2')
+const output3 = document.querySelector('#out3')
+const output4 = document.querySelector('#out4')
+const output5 = document.querySelector('#out5')
+const dateOutput = document.querySelector('#dateOut')
+
+// Calendar
 const date_picker_element = document.querySelector('.datePicker');
 const selected_date_element = document.querySelector('.datePicker .selectedDate');
 const dates_element = document.querySelector('.datePicker .dates');
@@ -6,11 +38,6 @@ const next_mnth_element = document.querySelector('.datePicker .dates .month .nxt
 const prev_mnth_element = document.querySelector('.datePicker .dates .month .prevmnth');
 const days_element = document.querySelector('.datePicker .dates .days');
 const months = [`January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`, `December`];
-
-
-// Declarations
-
-// Calendar
 let date = new Date();
 let day = date.getDate();
 let month = date.getMonth ();
@@ -24,18 +51,109 @@ let selectedYear = year
 mnth_element.textContent = months[month] + ` ` + year;
 selected_date_element.textContent = formatDate(date);
 selected_date_element.dataset.value = selectedDate
+dateOutput.innerHTML = selected_date_element.textContent
 
 // Section
-
 
 populateDates();
 
 //Event Listeners
+text1.addEventListener('input', outputValue1);
+text2.addEventListener('input', outputValue2);
+text3.addEventListener('input', outputValue3);
+text4.addEventListener('input', outputValue4);
+text5.addEventListener('input', outputValue5);
 
 date_picker_element.addEventListener ('click', toggleDatePicker);
 next_mnth_element.addEventListener('click', goToNextMonth);
 prev_mnth_element.addEventListener('click', goToPrevMonth);
-//Functions
+
+                         //Functions
+
+// Input display functions
+
+function outputValue1() {
+    output1.innerHTML = text1.value
+}
+function outputValue2() {
+    output2.innerHTML = text2.value
+}
+function outputValue3() {
+    output3.innerHTML = text3.value
+}
+function outputValue4() {
+    output4.innerHTML = text4.value
+}
+function outputValue5() {
+    output5.innerHTML = text5.value
+}
+                                                
+
+// Class Selector
+
+// function show(id) {
+// if(visibleId !== id) {
+//     visibleId = id;
+// }hide();
+// }
+// function hide() {
+// var div, i, id;
+// for(i = 0; i < divs.length; i++) {
+//     id = divs[i];
+//     div = document.getElementById(id);
+//     if(visibleId === id) {
+//     div.style.display = "block";
+//     } else {
+//     div.style.display = "none";
+//     }
+// }
+// }  
+
+trad.addEventListener('click',() => {
+    if (cardinv.innerText != cardinv.classList.contains('')){
+    cardinv.classList.remove( 'blacknGold', 'greennGold', 'dustyRose')
+    cardinv.classList.add('traditional')
+    one.classList.remove( 'blacknGold', 'greennGold', 'dustyRose')
+    one.classList.add('traditional')
+    const tradition = document.createElement('img');
+    tradition.src = 'Invitations/Traditional.jpeg'
+    cardinv.removeChild(cardinv.lastElementChild)
+    cardinv.appendChild(tradition)
+    }
+})
+
+black.addEventListener('click',() => {
+    if (cardinv.innerText != cardinv.classList.contains('')){
+    cardinv.classList.remove('traditional', 'greennGold', 'dustyRose')
+    cardinv.classList.add('blacknGold')
+    const tradition = document.createElement('img');
+    tradition.src = 'Invitations/Traditional.jpeg'
+    const blackGold = document.createElement('img');
+    blackGold.src = 'Invitations/BlacknGold.jpeg'
+    cardinv.removeChild(cardinv.lastElementChild)
+    cardinv.appendChild(blackGold)
+    }
+})
+green.addEventListener('click',() => {
+    if (cardinv.innerText != cardinv.classList.contains('')){
+    cardinv.classList.remove('traditional', 'blacknGold', 'dustyRose')
+    cardinv.classList.add('greennGold')
+    const emerald = document.createElement('img');
+    emerald.src = 'Invitations/GreennGold.jpeg'
+    cardinv.removeChild(cardinv.lastElementChild)
+    cardinv.appendChild(emerald)
+    }
+})
+rose.addEventListener('click',() => {
+    if (cardinv.innerText != cardinv.classList.contains('')){
+    cardinv.classList.remove('traditional', 'greennGold', 'blacknGold')
+    cardinv.classList.add('dustyRose')
+    const rosey = document.createElement('img');
+    rosey.src = 'Invitations/DustyRose.jpeg'
+    cardinv.removeChild(cardinv.lastElementChild)
+    cardinv.appendChild(rosey)
+    }
+})
 
 // Calendar
 function toggleDatePicker (e) {
@@ -85,8 +203,11 @@ function populateDates (e) {
             selectedMonth = month;
             selectedYear = year;
             selected_date_element.textContent = formatDate(selectedDate);
+            selected_date_element.dataset.value = selectedDate;
             populateDates()
             dates_element.classList.toggle('active');
+            dateOutput.innerHTML = selected_date_element.textContent
+
         })
         
         days_element.appendChild(day_element);
@@ -120,8 +241,4 @@ function formatDate(d) {
 
     return day + '/' + month + '/' + year
 }
-
-
-
-
 
